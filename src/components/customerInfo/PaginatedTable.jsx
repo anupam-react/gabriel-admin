4809346cc8b36ceaafc01899dbde2030c769d161
ANUPAM-RO <anupamroy575@gmail.com>
2024-03-20 16:37:03 +0530

    comment info

--- src/components/customerInfo/PaginatedTable.jsx
+++ src/components/customerInfo/PaginatedTable.jsx
@@ -4,7 +4,7 @@ import "./index.scss";
 import Pagination from "../common/Pagination";
 import MenuCard from "./MenuCard";
 import { DialogDefault } from "../common/DilogBox";
-import CustomeInfo from "./CustomeInfo";
+// import CustomeInfo from "./CustomeInfo";
 
 const PaginatedTable = () => {
   const [currentPage, setCurrentPage] = useState(1);
